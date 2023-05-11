b4dc8236ccb4bbe48ec987ae33d3b2a6416756f3
Oskar R <o.rajzner@gmail.com>
2023-05-11 17:36:29 +0200

    Init commit, add mvn build tool with spring dependencies

---
+++ src/main/java/com/viepovsky/CityWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.viepovsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityWeatherApplication.class, args);
+	}
+
+}
